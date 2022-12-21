776510927acc4d873a4ed3868aa311674ef0decb
DexterYBN <yakubov959@mail.ru>
2022-12-21 14:16:57 +0300

    сделал бек и сделал так чтобы при добавления комментария было видно время создания комментария

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -6,6 +6,10 @@ const commentSchema = mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
+  createdAt: {
+    type: Date,
+    default: Date.parse,
+  },
 });
 
 const Comment = mongoose.model("Comment", commentSchema);
