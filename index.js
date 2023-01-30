8383029325abd9dcb2d460a103ada7dadc94d673
DexterYBN <yakubov959@mail.ru>
2023-01-30 17:17:11 +0300

    /

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require("path");
 const app = express();
 
 app.use(morgan("dev"));
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname)));
 
@@ -18,6 +18,8 @@ app.use(require("./routes/users.route"));
 app.use(require("./routes/comments.route"));
 app.use(require("./routes/categories.route"));
 app.use(require("./routes/news.route"));
+
+mongoose.set('strictQuery', false);
 
 const startServer = async () => {
   try {
